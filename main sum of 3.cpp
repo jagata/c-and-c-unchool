c8298d1405680e0730e635f12abbef71b8a5ea6f
jagata <68096088+jagata@users.noreply.github.com>
2020-07-10 11:24:22 +0530

    Add files via upload

---
+++ main sum of 3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter three mumbers :");
+    scanf("%d%d%d",&a,&b,&c);
+    d=a+b+c;
+    printf("sum of a+b+c=%d",d);
+    return 0;
+    getch();
+
+
+}
