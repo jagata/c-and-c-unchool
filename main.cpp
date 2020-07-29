1721fa33efe1c8247b73f82f8212e3507cd2816d
jagata <68096088+jagata@users.noreply.github.com>
2020-07-29 12:05:28 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"enter no:";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"area of circle:"<<area;
+    return 0;
+}
+
