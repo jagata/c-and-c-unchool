ceb6d407e2625579c41a98156dc8e91881f6732b
jagata <68096088+jagata@users.noreply.github.com>
2020-07-29 11:56:14 +0530

    Add files via upload

---
+++ c++5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float area,l,b;
+    cout<<"enter l:";
+    cin>>l;
+    cout<<"enter b:";
+    cin>>b;
+    area=l*b;
+    cout<<"area of rectangle:"<<area;
+    return 0;
+}
---
+++ c++6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float area,h,b;
+    cout<<"enter h:";
+    cin>>h;
+    cout<<"enter b:";
+    cin>>b;
+    area=h*b/2;
+    cout<<"area of triangle:"<<area;
+    return 0;
+}
